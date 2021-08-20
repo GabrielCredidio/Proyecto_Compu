ac7720cf589bc809c846f98ab3c59144c210ad69
GabrielCredidio <88898446+GabrielCredidio@users.noreply.github.com>
2021-08-20 14:14:53 -0500

    Add files via upload

---
+++ proyecto.py
@@ -0,0 +1,10 @@
+"""
+EO(preguntas de 1-180, num1, num2)
+ var = num1 + num2 + num3
+ imprimir pregunta num1
+ respuesta usuario pregunta 1 = a
+ respuesta correcta pregunta 1 = d
+ print(correcto respuesta d)
+ print(incorrecto a,b,c)
+EF(print var) promedio del examen
+"""
