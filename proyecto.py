83cecc435db4e52f643435534bbb1e21a2148819
GabrielCredidio <88898446+GabrielCredidio@users.noreply.github.com>
2021-08-27 13:26:53 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -8,3 +8,49 @@ EO(preguntas de 1-180, num1, num2)
  print(incorrecto a,b,c)
 EF(print var) promedio del examen
 """
+
+
+"""
+def evalua_multiplicacion (num1, vnum2):
+    """
+    recibir --> num1 valor numérico, num2 valor numérico
+    multiplicar --> num1 * num2
+    devolver --> resultado de multiplicacion
+    """
+return num1 * num2
+
+def evalua_suma (num1, vnum2):
+    """
+    recibir --> num1 valor numérico, num2 valor numérico
+    suma --> num1 + num2
+    devolver --> resultado de suma
+    """
+return num1 + num2
+
+def evalua_division (num1, vnum2):
+    """
+    recibir --> num1 valor numérico, num2 valor numérico
+    dividir --> num1 / num2
+    devolver --> resultado de division
+    """
+return num1 / num2
+
+if repuesta (a) == True
+ respuesta = respuesta + 1
+print("respuesta 1 valor %i, es %r debe ser %r" % (a ,respuesta(a), True))
+
+if repuesta (c) == False
+ respuesta = respuesta + 0
+print("respuesta 2 valor %i, es %r debe ser %r" % (c ,respuesta(c), False))
+
+if repuesta (b) == True
+ respuesta = respuesta + 1
+print("respuesta 3 valor %i, es %r debe ser %r" % (b ,respuesta(b), True))
+
+if repuesta (d) == False
+ respuesta = respuesta + 0
+print("respuesta 4 valor %i, es %r debe ser %r" % (d ,respuesta(d), False))
+"""
+
+
+
