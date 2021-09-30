7914f94e9d8ae02013e15c88ce4e1876944d8d63
GabrielCredidio <88898446+GabrielCredidio@users.noreply.github.com>
2021-09-30 18:25:33 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -1,12 +1,3 @@
-EO(preguntas de 1-180, num1, num2)
- var = num1 + num2 + num3
- imprimir pregunta num1
- respuesta usuario pregunta 1 = a
- respuesta correcta pregunta 1 = d
- print(correcto respuesta d)
- print(incorrecto a,b,c)
-EF(print var) promedio del examen
-
 #===========================
 # em = evalua multiplicación
 def em():
@@ -19,7 +10,7 @@ def em():
 
 multiplicacion = em()
 print(multiplicacion)
-#==========================
+#===========================
 # es = evaluación suma
 def es ():
  
@@ -30,7 +21,7 @@ def es ():
 
 suma = es()
 print(suma)
-#==========================
+#===========================
 # ed = evaluación división
 def ed ():
     
@@ -40,46 +31,18 @@ def ed ():
     return resultado
 
 division = ed()
-print(suma)
-#==========================
-
-respuesta = input("Seleccione las letras entre a, b, c ó d: ")
+print(division)
+#===========================
+soluciones = ["d", "c", "c", "a"]
 R=0
-
-R = int(input("Ingresa una calificación: "))
-
-while R < 70:
-    R = int(input("Ingresa una calificación: "))
-
-if respuesta == "a":
-    R = R + 1
-    print("respuesta 1 valor %i, es %r debe ser %r" % (a ,respuesta(a), True))
-
-if respuesta == "b":
-    respuesta = respuesta + 0
-    print("respuesta 2 valor %i, es %r debe ser %r" % (c ,respuesta(c), False))
-
-if respuesta == "c":
-    respuesta = respuesta + 1
-    print("respuesta 3 valor %i, es %r debe ser %r" % (b ,respuesta(b), True))
-
-if respuesta == "d":
-    respuesta = respuesta + 0
-    print("respuesta 4 valor %i, es %r debe ser %r" % (d ,respuesta(d), False))
-
-def evalua(respuesta_correcta, respuesta_usuario, evaluacion, muestra = 1):
-    if respuesta == respuesta_usuario:
-        evaluacion = evaluacion +1
-    if muestra == 1:
-        respuesta(respuesta_correcta, respuesta_usuario, evaluacion)
-    return evaluacion 
-  
-def respuesta(respuesta_correcta, respuesta_usuario,evaluacion):
- 
-    print("Respuesta correcta.", respuesta)
-    print("Respuesta:", respuesta_usuario)
-    print("Evaluacion_en_linea:", evaluacion)
-    
+for i in soluciones:
+    respuesta = input("Selecione las letras entre a, b, c, d: ")
+    if respuesta ==1:
+        R = R + 1
+        print("La respuesta es correcta.", R, "puntos acumulados de",len(soluciones),"puntos.")
+    elif respuesta != i:
+        print("Respuesta incorrecta. La respuesta correcta era:", i)
+#===========================   
 R = int(input("Ingresa una calificación de Matematicas: "))
 
 while R < 70:
@@ -95,4 +58,3 @@ R = int(input("Ingresa una calificación Computacion: "))
 
 while R < 70:
     R = int(input("Ingresa una calificación: "))
-
