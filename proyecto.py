57964a784563c9414a15114d0fe096ff164fbb1d
GabrielCredidio <88898446+GabrielCredidio@users.noreply.github.com>
2021-10-01 18:34:22 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -33,15 +33,15 @@ def ed ():
 division = ed()
 print(division)
 #===========================
-soluciones = ["d", "c", "c", "a"]
+soluciones = ["d","c","c","a"]
 R=0
 for i in soluciones:
-    respuesta = input("Selecione las letras entre a, b, c, d: ")
-    if respuesta ==1:
+    respuesta = input("Seleccione las letras entre a, b, c ó d: ")
+    if respuesta == i:
         R = R + 1
-        print("La respuesta es correcta.", R, "puntos acumulados de",len(soluciones),"puntos.")
+        print("La respuesta es correcta.",R,"puntos acumulados de",len(soluciones),"puntos.")
     elif respuesta != i:
-        print("Respuesta incorrecta. La respuesta correcta era:", i)
+        print("Respuesta incorrecta. La respuesta correcta era",i)
 #===========================   
 R = int(input("Ingresa una calificación de Matematicas: "))
 
@@ -58,3 +58,20 @@ R = int(input("Ingresa una calificación Computacion: "))
 
 while R < 70:
     R = int(input("Ingresa una calificación: "))
+#===========================   
+# Código para generar una tabla de calificaciones
+# en función de los datos ingresados
+a = ["Materia     ","Calificación","Estado     "]
+b = ["Matemáticas","Español    ","computación"]
+d = []
+for i in b:
+    c = int(input("Ingrese la calificación de "+i+":\t"))
+    if c >= 70:
+        estado = "Aprobado"
+    else:
+        estado = "Reprobado"
+    d.append([i,c,estado])
+tabla = [a]+d
+
+for i in tabla:
+    print(i[0],i[1],i[2]) 
